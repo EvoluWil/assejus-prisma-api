8b732315d4c9eac5d3d85f5c5dc50ae542753a2c
willian-rodrigues <willian.rsilva1994@gmail.com>
2022-02-15 23:04:05 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,22 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT);
+
+  app.enableCors({
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: [
+      'X-Requested-With',
+      'X-HTTP-Method-Override',
+      'Content-Type',
+      'Accept',
+      'Observe',
+      'X-Authorization',
+      'X-Token-Auth',
+      'Authorization',
+    ],
+    methods: 'GET, POST, PUT, DELETE, UPDATE, OPTIONS',
+  });
 }
 bootstrap();
