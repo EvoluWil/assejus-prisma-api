793b0eafc96917e193af447b0e45693272369a04
willian-rodrigues <willian.rsilva1994@gmail.com>
2022-02-15 23:06:42 -0300

    cors config

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
-
   app.enableCors({
     credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204,
+    origin: 'http://assejusonline.com.br',
     allowedHeaders: [
       'X-Requested-With',
       'X-HTTP-Method-Override',
@@ -21,5 +20,6 @@ async function bootstrap() {
     ],
     methods: 'GET, POST, PUT, DELETE, UPDATE, OPTIONS',
   });
+  await app.listen(process.env.PORT);
 }
 bootstrap();
